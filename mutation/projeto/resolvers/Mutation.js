03099118901a594d74bfcca7da9fc771bebfb286
angelocastro9 <castromatheus060@gmail.com>
2019-05-21 10:53:52 -0300

    input

--- mutation/projeto/resolvers/Mutation.js
+++ mutation/projeto/resolvers/Mutation.js
@@ -1,13 +1,25 @@
 const { usuarios, proximoId } = require('../data/db')
 
+function indiceUsuario(filtro) {
+    if (!filtro) return -1
+    const { id, email } = filtro
+    if (id) {
+        return usuarios.findIndex(u => u.id === id)
+    }
+    else if (email) {
+        return usuarios.findIndex(u => u.email === email)
+    }
+    return -1
+}
+
 module.exports = {
     // { nome, email, idade }
     novoUsuario(_, { dados }) {
         const emailExistente = usuarios.some(u => u.email === dados.email)
-        if(emailExistente) {
+        if (emailExistente) {
             throw new Error('E-mail cadastro')
-        } 
-        
+        }
+
         const novo = {
             id: proximoId(),
             ...dados,
@@ -17,20 +29,20 @@ module.exports = {
         usuarios.push(novo)
         return novo
     },
-    excluirUsuario(_, {id}) {
-        const i = usuarios.findIndex(u => u.id === id)
-        if( i < 0 ) return null
+    excluirUsuario(_, { id }) {
+        const i = indiceUsuario(filtro)
+        if (i < 0) return null
         const excluidos = usuarios.splice(i, 1)
         return excluidos ? excluidos[0] : null
     },
 
     alterarUsuario(_, args) {
         const i = usuarios.findIndex(u => u.id === args.id)
-        if(i < 0) return null
+        if (i < 0) return null
 
         usuarios[i].nome = args.nome
         usuarios[i].email = args.email
-        if(args.idade) {
+        if (args.idade) {
             usuarios[i].idade = args.idade
         }
 
@@ -41,6 +53,6 @@ module.exports = {
 
         // usuarios.splice(i, 1, usuario)
         // return usuario
-        return  usuarios[i]
+        return usuarios[i]
     }
 }
