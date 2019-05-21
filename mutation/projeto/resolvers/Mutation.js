3d32fe0c234bf5963fb2f4ecb5fc393583a6aa48
angelocastro9 <castromatheus060@gmail.com>
2019-05-21 10:45:09 -0300

    input

--- mutation/projeto/resolvers/Mutation.js
+++ mutation/projeto/resolvers/Mutation.js
@@ -2,15 +2,15 @@ const { usuarios, proximoId } = require('../data/db')
 
 module.exports = {
     // { nome, email, idade }
-    novoUsuario(_, args) {
-        const emailExistente = usuarios.some(u => u.email === args.email)
+    novoUsuario(_, { dados }) {
+        const emailExistente = usuarios.some(u => u.email === dados.email)
         if(emailExistente) {
             throw new Error('E-mail cadastro')
         } 
         
         const novo = {
             id: proximoId(),
-            ...args,
+            ...dados,
             perfil_id: 1,
             status: 'ATIVO'
         }
