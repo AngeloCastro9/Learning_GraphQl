6946078f6b82480c9857267914063975025cb168
angelocastro9 <castromatheus060@gmail.com>
2019-05-23 14:49:55 -0300

    Primeira migration

--- banco-de-dados/projeto/migrations/20190523144148_tabela_perfis.js
+++ banco-de-dados/projeto/migrations/20190523144148_tabela_perfis.js
@@ -1,8 +1,12 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('perfis', table => {
+      table.increments('id').primary()
+      table.string('nome').notNull().unique()
+      table.string('rotuno').notNull()
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('perfis')
 };
